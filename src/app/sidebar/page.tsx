368c1379413a1a72ea8a192f82e044fde4f22ebc
kamlesh-Sahani <alex20150001969@gmail.com>
2024-08-12 02:20:45 +0530

    user layout page

---
+++ src/app/sidebar/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserSidebar from '@/components/user/UserSidebar'
+const page = () => {
+  return (
+    <div>
+ <UserSidebar />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/user/UserSidebar.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+export default function UserSidebar() {
+  return (
+    <Sidebar
+      aria-label="Sidebar with logo branding example"
+      style={{ width: '250px', height: '100vh' }} // Adjust width and height as needed
+    >
+      <Sidebar.Logo href="#" img="" imgAlt="Flowbite logo">
+        Flowbite
+      </Sidebar.Logo>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            Kanban
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiInbox}>
+            Inbox
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
