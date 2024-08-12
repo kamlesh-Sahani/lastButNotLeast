763a383982e1f247d6761818c324102da6e7ebb8
Aq7853gehc <aquib7583@gmail.com>
2024-08-12 23:02:29 +0530

    settign

--- src/app/user/setting/page.tsx
+++ src/app/user/setting/page.tsx
@@ -255,7 +255,7 @@ const SettingsPage = () => {
               <input
                 id="document"
                 type="file"
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full  border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 onChange={handleDocumentUpload}
               />
               {document && (
