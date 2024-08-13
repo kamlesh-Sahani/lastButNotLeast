cefb8c35a227dc3d425ceac7cc05897f3bb26713
kamlesh-Sahani <alex20150001969@gmail.com>
2024-08-13 17:59:42 +0530

    admin dashboard

--- src/components/simple.jsx
+++ src/components/simple.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
 const simple = () => {
+  const leaveTrendData = {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+  }
   return (
-    <div>simple</div>
+    <div>
+
+
+
+<div className="bg-white p-6 rounded-lg shadow-lg">
+            <h2 className="text-2xl font-bold text-gray-700 mb-4">
+              Monthly Leave Requests
+            </h2>
+            <Bar
+              data={{
+                labels: leaveTrendData.labels,
+                datasets: [
+                  {
+                    label: "Leave Requests",
+                    data: leaveTrendData.datasets[0].data,
+                    backgroundColor: "#2196f3",
+                  },
+                ],
+              }}
+            />
+          </div>
+    </div>
   )
 }
 
