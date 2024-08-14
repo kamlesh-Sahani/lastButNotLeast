ba97cef8cda44585899b58fcf21988db377c1468
tahirkhan-01 <tahirkhan896y@gmail.com>
2024-08-14 13:13:27 +0530

    edit profile

--- src/app/admin/employee/page.tsx
+++ src/app/admin/employee/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 import { useState } from "react";
-import EmployeeCard from "./component/EmployeeCard";
+import EmployeeCard from "../../../components/admin/EmployeeCard";
 import { LuSearch } from "react-icons/lu";
 import Link from "next/link";
 
