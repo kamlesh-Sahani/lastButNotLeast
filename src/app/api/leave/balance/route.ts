53ae1d2c962a177bf75a74fb89560b41fbdb52a0
kamlesh-Sahani <alex20150001969@gmail.com>
2024-08-15 21:10:30 +0530

    apis updates

--- src/app/api/leave/balance/route.ts
+++ src/app/api/leave/balance/route.ts
@@ -1,29 +1,28 @@
-// import LeaveBalanceModel from "@/src/models/LeaveBalance.model";
-// import { NextResponse,NextRequest } from "next/server";
-// import LeaveTypesModel from "@/src/models/LeaveType.model";
-// import dbConnect from "@/src/app/lib/dbConnect";
-// import LeaveApplicationModel from "@/src/models/LeaveApplication.model";
-// import LeaveApprovalModel from "@/src/models/LeaveApproval.model";
-// dbConnect();
+import { NextResponse,NextRequest } from "next/server";
+import LeaveBalanceModel from "@/models/LeaveBalance.model";
+import LeaveTypesModel from "@/models/LeaveType.model";
+import LeaveApplicationModel from "@/models/LeaveApplication.model";
+import dbConnect from "@/lib/dbConnect";
+dbConnect();
 
-// export async function POST(req:NextRequest){
-//     try {
-//         const searchParams =  req.nextUrl.searchParams;
-//         const id = searchParams.get("id");
+export async function POST(req:NextRequest){
+    try {
+        const searchParams =  req.nextUrl.searchParams;
+        const id = searchParams.get("id");
 
-//         const leaveApplication = await LeaveApplicationModel.find({employeeId:id,status:"APPROVED"});
+        const leaveApplication = await LeaveApplicationModel.find({employeeId:id,status:"APPROVED"});
 
         
-//         //leave types 
-//         const LeaveTypes = await LeaveTypesModel.find({});
+        //leave types 
+        const LeaveTypes = await LeaveTypesModel.find({});
 
-//         const reqBody = await req.json();
-//         const {} = reqBody;
-//         const leaveBalance = await LeaveBalanceModel.create({})
-//     } catch (error:any) {
-//        return NextResponse.json({
-//         success:false,
-//         message:`something went error : ${error.message}`
-//        }) 
-//     }
-// }
+        const reqBody = await req.json();
+        const {} = reqBody;
+        const leaveBalance = await LeaveBalanceModel.create({})
+    } catch (error:any) {
+       return NextResponse.json({
+        success:false,
+        message:`something went error : ${error.message}`
+       }) 
+    }
+}
