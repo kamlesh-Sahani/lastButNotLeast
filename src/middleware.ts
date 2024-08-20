3359bd6d7a12864dcf757b058ec37e31cc931c88
kamlesh sahani <alex20150001969@gmail.com>
2024-08-20 23:33:51 +0530

    no changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
   const loggedUserNotAccessPath = request.nextUrl.pathname === "/login";
+  
   const isLogoutPath = request.nextUrl.pathname === "/logout";
   const protectedRoutes =
     request.nextUrl.pathname === "/user" ||
