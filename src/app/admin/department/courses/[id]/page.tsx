02b30fb3b23723c12128df0ff4df6d5340286045
Aq7853gehc <aquib7583@gmail.com>
2024-09-01 21:28:03 +0530

    sjfkdsl

--- src/app/admin/department/courses/[id]/page.tsx
+++ src/app/admin/department/courses/[id]/page.tsx
@@ -1,18 +1,19 @@
-import { Card } from "flowbite-react"
-import { dumy } from "../../page"
-import { useState } from "react"
-import { DepartmentSchemaType } from "@/models/Department.model"
+import { Card } from "flowbite-react";
+import { dumy } from "../../page";
+import { useState } from "react";
+import { DepartmentSchemaType } from "@/models/Department.model";
 
-const Courses = (params:{id:string}) => {
-  
-  const [department, setDepartment] = useState<DepartmentSchemaType[]>(dumy)
-  const courseName = department.mapw()
+const Courses = (params: { id: string }) => {
+  const [department, setDepartment] = useState<DepartmentSchemaType[]>(dumy);
+  // const courseName = department.map()
   return (
     <div className="flex flex-wrap gap-3 ">
       <Card>
-        <h5 className="text-xl font-medium text-gray-900 dark:text-white">Course Name</h5>
+        <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+          Course Name
+        </h5>
       </Card>
     </div>
-  )
-}
-export default Courses
+  );
+};
+export default Courses;
